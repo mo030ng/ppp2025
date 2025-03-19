bd48288e0f9b1eca144b6b838f19dc06480c205f
mo030ng <bma101372@gmail.com>
2025-03-19 15:22:44 +0900

    Add files via upload

---
+++ hw02/hw02_1.py
@@ -0,0 +1,7 @@
+temp_c=30
+temp_f=(temp_c)*9/5+32
+print("섭씨{}도는 화씨로 {}도이다.".format(temp_c,temp_f))
+
+temp_c=0
+temp_f=(temp_c)*9/5+32
+print("섭씨{}도는 화씨{}도이다.".format(temp_c,temp_f))
---
+++ hw02/hw02_2.py
@@ -0,0 +1,5 @@
+weight=60
+height=170 
+height_m= height/100
+BMI=weight/height_m**2
+print("키는 {}, 몸무게는 {}, BMI= {}". format(height,weight,BMI))
