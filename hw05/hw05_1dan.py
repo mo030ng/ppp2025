506b64370e7ea59a9ca1754906b056dbb7b30fa6
mo030ng <bma101372@gmail.com>
2025-03-29 13:49:49 +0900

    Delete hw05 directory

--- hw05/hw05_1dan.py
+++
@@ -1,4 +0,0 @@
-dan = int(input("단을 입력하시오: "))
-
-for i in range(9):
-    print(f"{dan} * {i+1} = {dan*(i+1)}")
--- hw05/hw05_2star.py
+++
@@ -1,4 +0,0 @@
-n = int(input("n값을 입력하세요 : "))
-
-for i in range(n):
-    print ("*"*(i+1))
